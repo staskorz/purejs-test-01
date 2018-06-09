692eba31d81ef4c9d00cd03fc0291a68f87fe2e3
staskorz <staskorz@gmail.com>
2018-06-09 23:24:36 +0300

    Added item selection functionality

--- src/element-creators/name.js
+++ src/element-creators/name.js
@@ -1,15 +1,37 @@
+const SELECTED_CLASS_NAME = "selected";
+
+const state = {
+  selectedElement: null
+};
+
+const toggleSelection = ({ element, selectedElement }) => {
+  if (element === selectedElement) {
+    element.classList.remove(SELECTED_CLASS_NAME);
+    state.selectedElement = null;
+  } else {
+    if (selectedElement) {
+      selectedElement.classList.remove(SELECTED_CLASS_NAME);
+    }
+
+    state.selectedElement = element;
+    element.classList.add(SELECTED_CLASS_NAME);
+  }
+};
+
 export default ({ name, onClick }) => {
   const div = document.createElement("div");
   const text = document.createTextNode(name);
   div.appendChild(text);
 
-  if (onClick) {
-    div.addEventListener("click", e => {
-      e.preventDefault();
+  div.addEventListener("click", e => {
+    e.preventDefault();
 
+    toggleSelection({ element: div, selectedElement: state.selectedElement });
+
+    if (onClick) {
       onClick();
-    });
-  }
+    }
+  });
 
   return div;
 };
